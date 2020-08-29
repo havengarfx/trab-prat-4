f876e707215dd76f4e5d41d505a951842e742f55
havengarfx <havengarfx@gmail.com>
2020-08-29 00:20:51 +0000

    mongo atlas

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import gradesRouter from './routes/grades.js';
 import { promises as fs } from 'fs';
+import mongoose from 'mongoose';
+import { accountModel } from './models/accountmodel.js';
 
 const { readFile, writeFile } = fs;
 
@@ -8,23 +10,28 @@ const app = express();
 app.use(express.json());
 
 app.use('/grades', gradesRouter);
+(async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://dbUser:dbUserPass@cluster0.aba5z.mongodb.net/bank?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log('Conectado no MongoDB');
+  } catch (error) {
+    console.log('Erro ao conectar no MongoDB');
+  }
+})();
 
 app.listen(3000, async () => {
-  try {
-    const initialJson = {
-      nextId: 1,
-      grades: [],
-    };
-    await readFile('grades.json').then(() => {
-      console.log('API Started');
-    });
-  } catch (error) {
-    await writeFile('grades.json', JSON.stringify(initialJson))
-      .then(() => {
-        console.log('API Started');
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+  const data = JSON.parse(await readFile('accounts-3.json'));
+  const{agencia, conta, name, balance}=data;
+for (let index = 0; index < data.length; index++) {
+  
+  
+
+  const account = new accountModel({agencia:agencia,conta:conta,name:name,balance:balance});
+  account.save();}
 });
